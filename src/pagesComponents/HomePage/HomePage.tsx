3c2762cd2119229ac6844a5e2180d0d7c9364609
Slawone <for1s8@ya.ru>
2026-03-03 10:56:07 +0300

    Fixed module

--- src/pagesComponents/HomePage/HomePage.tsx
+++ src/pagesComponents/HomePage/HomePage.tsx
@@ -1,15 +1,9 @@
 'use client';
 
-import { useRef } from 'react';
-import { useInView } from 'framer-motion';
-import { motion } from 'framer-motion';
 import { Announcement } from './components';
 import { System } from './components';
 
 export const HomePage = () => {
-  const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
-
   return (
     <>
       <Announcement />
