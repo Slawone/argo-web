077a4b8bc74c2ce428be8e7357f2546c7a5a1683
Slawone <for1s8@ya.ru>
2026-04-08 08:38:45 +0300

    Fixed module

--- src/sections/home/Tabs.jsx
+++ src/sections/home/Tabs.jsx
@@ -13,16 +13,25 @@ const tabs = [
     label: "Storage",
     leftText: (
       <>
-        Объединённая судостроительная корпорация (ОСК) существенно оптимизировала процессы резервного копирования данных. Благодаря внедрению системы хранения данных АРГО с полезным объёмом свыше 1 ПБ удалось добиться двукратного ускорения работы <span className="title-color">Veeam Backup.</span>
+        <a href="https://www.tadviser.ru/index.php/%D0%9F%D1%80%D0%BE%D0%B5%D0%BA%D1%82:%D0%9E%D0%B1%D1%8A%D0%B5%D0%B4%D0%B8%D0%BD%D0%B5%D0%BD%D0%BD%D0%B0%D1%8F_%D1%81%D1%83%D0%B4%D0%BE%D1%81%D1%82%D1%80%D0%BE%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F_%D0%BA%D0%BE%D1%80%D0%BF%D0%BE%D1%80%D0%B0%D1%86%D0%B8%D1%8F_%28%D0%9E%D0%A1%D0%9A%29_%28Argo.Tech_Storage_%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%BD%D1%8B%D0%B9_%D0%BA%D0%BE%D0%BC%D0%BF%D0%BB%D0%B5%D0%BA%D1%81_%D1%85%D1%80%D0%B0%D0%BD%D0%B5%D0%BD%D0%B8%D1%8F_%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85%29?ysclid=mnmtcx10m0328713095"
+        target="_blank"
+        className="inline-flex items-center gap-1 title-color"
+        >
+          Объединённая судостроительная корпорация (ОСК)
+          <ExternalLink width={18} height={18} />
+        </a>{" "}
+        существенно оптимизировала процессы резервного копирования данных.
+        Благодаря внедрению системы хранения данных АРГО с полезным объёмом
+        свыше 1 ПБ удалось добиться двукратного ускорения работы{" "}
+        <span className="title-color">Veeam Backup.</span>
       </>
     ),
     rightText: (
       <>
-        Продукты{" "}
-        <span className="title-color uppercase">Argo.tech</span>{" "}
+        Продукты <span className="title-color uppercase">Argo.tech</span>{" "}
         разрабатываются в России и сопровождаются
-        <span className="title-color uppercase"> до 10 лет</span>,
-        обеспечивая стабильную работу инфраструктуры на всём жизненном цикле.
+        <span className="title-color uppercase"> до 10 лет</span>, обеспечивая
+        стабильную работу инфраструктуры на всём жизненном цикле.
       </>
     ),
   },
