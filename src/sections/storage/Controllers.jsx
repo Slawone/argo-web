e0cac6891997718e812c926d11b9fd890ef32bdf
Slawone <for1s8@ya.ru>
2026-04-16 12:09:26 +0300

    Fixed module

--- src/sections/storage/Controllers.jsx
+++ src/sections/storage/Controllers.jsx
@@ -7,10 +7,13 @@ export const Controllers = () => {
       <div className="grid md:grid-cols-[7fr_5fr]">
         {/* left */}
         <div className="flex flex-col items-start gap-8 font-light section-py px-4 md:border-r md:border-r-black/8 md:dark:border-white/14">
-          <p className="">
-            Нет единой точки отказа. Контроллеры системы хранения обеспечивают балансировку нагрузки и автоматический Failover (аварийное переключение), поддерживая непрерывный доступ к данным.
+          <p className="text-lg md:text-xl lg:text-2xl font-light text-color">
+            <span className="title-color">Нет единой точки отказа.</span>{" "}
+            Контроллеры системы хранения обеспечивают балансировку нагрузки и
+            автоматический Failover (аварийное переключение), поддерживая
+            непрерывный доступ к данным.
           </p>
-          <ul className="">
+          <ul className="main-text font-light text-color flex flex-col gap-4 max-w-150">
             <li className="flex gap-2 items-baseline">
               <Image
                 src="/assymetric-arrow.svg"
@@ -18,25 +21,33 @@ export const Controllers = () => {
                 height={12}
                 alt=""
               />
-              Каждый сервер имеет несколько независимых путей к данным.Поддерживается Asymmetric Logical Unit Access (ALUA) — технология, обеспечивающая оптимальный выбор активного пути и мгновенное переключение при сбое.
+              <p>
+                Каждый сервер имеет несколько независимых путей к данным.
+                Поддерживается{" "}
+                <span className="title-color">
+                  Asymmetric Logical Unit Access
+                </span>{" "}
+                (ALUA) - технология, обеспечивающая оптимальный выбор активного
+                пути и мгновенное переключение при сбое.
+              </p>
             </li>
             <li className="flex gap-2 items-baseline">
-              <Image
-                src="/lightning.svg"
-                width={12}
-                height={12}
-                alt=""
-              />
-              Непрерывный доступ: потеря кабеля или порта не вызывает простоя.Система выполняет автоматическое перераспределение запросов по всем доступным линиям.
+              <Image src="/lightning.svg" width={12} height={12} alt="" />
+              <p>
+                <span className="title-color">Непрерывный доступ:</span> потеря
+                кабеля или порта не вызывает простоя.Система выполняет
+                автоматическое перераспределение запросов по всем доступным
+                линиям.
+              </p>
             </li>
             <li className="flex gap-2 items-baseline">
-              <Image
-                src="/shield.svg"
-                width={12}
-                height={12}
-                alt=""
-              />
-              Двойная надёжность: каждая операция выполняется по нескольким путям одновременно, что повышает отказоустойчивость и стабильность системы хранения данных.
+              <Image src="/shield.svg" width={12} height={12} alt="" />
+              <p>
+                <span className="title-color">Двойная надёжность:</span> каждая
+                операция выполняется по нескольким путям одновременно, что
+                повышает отказоустойчивость и стабильность системы хранения
+                данных.
+              </p>
             </li>
           </ul>
         </div>
