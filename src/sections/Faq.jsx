2ae2529e5f685823b7ed08afa07ceaab531fa9c4
Slawone <for1s8@ya.ru>
2026-04-16 15:47:22 +0300

    Fixed module

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -7,7 +7,7 @@ export const Faq = () => {
   return (
     <section className="page-container border border-black/8 dark:border-white/14 grid md:grid-cols-2">
       <div className="section-py px-4 md:border-r md:border-r-black/8 md:dark:border-white/14">
-        <h2 className="text-base md:text-xl lg:text-[28px]font-light uppercase leading-none title-color">Faq</h2>
+        <h2 className="text-base md:text-xl lg:text-[28px] font-light uppercase leading-none title-color">Faq</h2>
       </div>
       <div className="">
         <Accordian items={faqStorage} className="" />
