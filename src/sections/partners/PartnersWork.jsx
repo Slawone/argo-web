cd22ba660a61a84a5d172ad35bad0129db52bcd4
Slawone <for1s8@ya.ru>
2026-05-28 23:13:39 +0300

    Fixed module

--- src/sections/partners/PartnersWork.jsx
+++ src/sections/partners/PartnersWork.jsx
@@ -2,24 +2,22 @@ import Image from "next/image";
 
 export const PartnersWork = () => {
   return (
-    <section className="relative page-container section-border font-sans main-text text-color">
-      <h3 className="mx-4 text-center mt-5 text-base md:text-xl lg:text-[28px] md:absolute md:w-150 lg:w-250 md:left-1/2 md:-translate-x-1/2 md:mt-7">
+    <section className="relative page-container section-border font-sans font-light text-color">
+      <h3 className="mx-4 text-center mt-5 text-base md:text-xl md:absolute md:w-150 lg:w-250 md:left-1/2 md:-translate-x-1/2 md:mt-7">
         <span className="title-color">Мы работаем</span> с технологическими
-        партнёрами по прозрачной лицензионной модели, обеспечивая доступность
+        партнёрами <br /> по прозрачной лицензионной модели, обеспечивая доступность
         наших решений через каналы широкой дистрибуции.
       </h3>
-      <div className="grid md:grid-cols-3 text-center">
-        <div className="px-4 py-5 flex flex-col items-center gap-10 md:pt-30 lg:pt-40">
+      <div className="grid md:grid-cols-2 text-center">
+        <div className="px-4 py-15 flex flex-col md:items-center gap-2 md:gap-5 md:pt-30 lg:pt-40">
           <div className="h-[239px]">
             <Image src="/partnersWork/ai.svg" width={297} height={207} alt="" />
           </div>
           <p className="text-left">
-            <span className="title-color">Открыты к взаимодействию </span>с
-            партнёрами и заказчиками, помогаем в подборе технологического
-            партнёра и выборе решения.
+            Мы открыты к взаимодействию с партнерами и заказчиками и предлагаем <span className="title-color">полный цикл услуг</span> по подбору технологического партнера и выбору оптимального решения - от анализа потребностей до запуска системы. 
           </p>
         </div>
-        <div className="border-black/8 dark:border-white/14 md:border-r md:border-l px-4 flex flex-col items-center gap-10 md:pt-30 lg:pt-40">
+        <div className="border-black/8 dark:border-white/14 md:border-r md:border-l px-4 flex flex-col md:items-center gap-2 md:gap-5 md:pt-30 lg:pt-40">
           <div className="h-[239px]">
             <Image
               src="/partnersWork/chat.svg"
@@ -28,22 +26,8 @@ export const PartnersWork = () => {
               alt=""
             />
           </div>
-          <p className="text-left">
-            <span className="title-color">Поддержка</span> при подборе решения
-            от направления до архитектуры, от выбора до инсталляции.
-          </p>
-        </div>
-        <div className="px-4 py-5 flex flex-col items-center gap-10 md:pt-30 lg:pt-40">
-          <div className="h-[239px]">
-            <Image
-              src="/partnersWork/schema.svg"
-              width={292}
-              height={230}
-              alt=""
-            />
-          </div>
-          <p className="text-left">
-            Мы стремимся сделать наши продукты <span className="title-color">максимально доступными</span> для бизнеса любого масштаба и локации. Для этого сформирована многоступенчатая сеть партнёров и дистрибьюторов.
+          <p className="text-left mb-5">
+            <span className="title-color">Поддержка</span> при подборе решения  от направления до архитектуры, от выбора до инсталляции. Мы не просто продаем решения, наша команда сопровождает вас на каждом этапе, наша цель - обеспечить идеальное соответствие решения вашим бизнес задачам и технической инфраструктуре. 
           </p>
         </div>
       </div>
