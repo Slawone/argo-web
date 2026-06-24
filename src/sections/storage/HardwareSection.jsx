8eff92acfb6ca54d8df533c204c1e939ec5375cc
Slawone <for1s8@ya.ru>
2026-06-24 16:21:06 +0300

    Fixed module

--- src/sections/storage/HardwareSection.jsx
+++ src/sections/storage/HardwareSection.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
+import { motion } from 'framer-motion';
 import { cn } from "@/utils";
 
 const steps = [
@@ -10,7 +11,7 @@ const steps = [
     icon: "/storage/meter.svg",
     iconActive: "/storage/meter-active.svg",
     title: "Визуализация",
-    titleColor: 'text-[#34C759]',
+    titleColor: "text-[#34C759]",
     text: "Отображение состояния оборудования и программных модулей в едином интерфейсе. Интуитивное управление с встроенной защитой повышает прозрачность и контроль за системой хранения",
   },
   {
@@ -19,7 +20,7 @@ const steps = [
     icon: "/storage/chart.svg",
     iconActive: "/storage/chart-active.svg",
     title: "Диагностика",
-    titleColor: 'text-[#007AFF]',
+    titleColor: "text-[#007AFF]",
     text: "Модуль расширенной самодиагностики, позволяющий выполнять мониторинг работоспособности компонентов программного обеспечения с точностью до сервиса",
   },
   {
@@ -28,54 +29,68 @@ const steps = [
     icon: "/storage/arrow-up.svg",
     iconActive: "/storage/arrow-up-active.svg",
     title: "Интерфейс",
-    titleColor: 'text-[#007AFF]',
+    titleColor: "text-[#007AFF]",
     text: "Графический интерфейс для управления комплексом хранения данных, позволяет создавать, удалять пулы из установленных жестких дисков, менять размер созданных пулов и томов",
   },
 ];
 
 export const HardwareSection = () => {
   const [active, setActive] = useState(0);
-  const [fading, setFading] = useState(false);
+  const [isAutoPlaying, setIsAutoPlaying] = useState(true);
 
   const handleSelect = (index) => {
-    if (index === active) return;
-    setFading(true);
-    setTimeout(() => {
-      setActive(index);
-      setFading(false);
-    }, 150);
+    setActive(index);
+    setIsAutoPlaying(false);
+    setTimeout(() => setIsAutoPlaying(true), 5000);
   };
 
   return (
     <>
       <section className="md:hidden page-container section-border section-py px-4 flex flex-col gap-15">
         {steps.map((step, i) => (
-            <div key={i} className="flex flex-col">
-              <h3 className="text-base font-light uppercase leading-none title-color mb-5">{step.title}</h3>
-              <p className="text-color main-text font-light mb-10">{step.text}</p>
-              <Image 
-                src={step.image}
-                width={300}
-                height={100}
-                alt=""
-                className="w-[80%] self-center"
-              />
-            </div>
+          <div key={i} className="flex flex-col">
+            <h3 className="text-base font-light uppercase leading-none title-color mb-5">
+              {step.title}
+            </h3>
+            <p className="text-color main-text font-light mb-10">{step.text}</p>
+            <Image
+              src={step.image}
+              width={300}
+              height={100}
+              alt=""
+              className="w-[80%] self-center"
+            />
+          </div>
         ))}
       </section>
-      <section className="hidden page-container section-border px-4">
+      <section className="hidden md:block page-container section-border px-4">
         <div className="flex justify-between">
-          <div className="section-py">
-            <Image src={steps[active].image} width={618} height={100} alt="" />
+          <div className="section-py flex flex-col items-start">
+              <motion.div
+                key={active}
+                initial={{ opacity: 0, x: 10 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x:  10 }}
+                transition={{ duration: 0.5 }}
+                className="w-[70%] xl:w-[90%] xl:self-center"
+              >
+                <Image
+                  src={steps[active].image}
+                  width={618}
+                  height={100}
+                  alt=""
+                  className="w-full"
+                />
+              </motion.div>
           </div>
-          <div className="flex flex-col gap-30 relative section-py">
-            <div className="hidden md:block absolute z-0 left-4.5 top-0 h-full w-0.5 -translate-x-1/2 rounded-full bg-linear-to-b from-[#34C759] to-[#007AFF]" />
+          <div className="flex flex-col gap-10 lg:gap-15 xl:gap-20 relative section-py">
+            <div className="hidden md:block absolute z-0 -left-15 top-0 h-full w-0.5 -translate-x-1/2 rounded-full bg-linear-to-b from-[#34C759] to-[#007AFF]" />
             {steps.map((step, i) => (
               <div key={i} className="flex items-start gap-15 z-10">
                 <button
                   onClick={() => handleSelect(i)}
                   aria-label={step.title}
-                  className="mt-1 cursor-pointer"
+                  className="mt-1 cursor-pointer absolute -left-19.5"
                 >
                   <Image
                     src={active === i ? step.iconActive : step.icon}
@@ -86,10 +101,15 @@ export const HardwareSection = () => {
                 </button>
 
                 <div className="max-w-105">
-                  <h3 className={cn(
-                    active === i ? step.titleColor : 'text-color'
-                  )}>{step.title}</h3>
-                  <p className="text-color">{step.text}</p>
+                  <h3
+                    className={cn(
+                      active === i ? step.titleColor : "text-color",
+                      "text-lg md:text-xl lg:text-2xl font-light uppercase leading-none mb-5",
+                    )}
+                  >
+                    {step.title}
+                  </h3>
+                  <p className="text-color main-text font-light">{step.text}</p>
                 </div>
               </div>
             ))}
