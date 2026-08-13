1701d7a4c9d9ae2e7d0835e13f223272f87aa099
Slawone <for1s8@ya.ru>
2026-08-13 20:54:34 +0300

    Add metadata for main page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,20 +15,35 @@ const oswald = Oswald({
 });
 
 export const metadata = {
+  metadataBase: new URL("https://argo.tech"),
   title: "СХД РФ: Реестр Минцифры, Минпромторга для импортозамещения",
   description:
-    "ARGO.TECH разрабатывает сертифицированное программное обеспечение для хранения и обработки данных с 2016 года. Решения для корпоративных и государственных структур: безопасность, надёжность, соответствие стандартам РФ.",
+    "Российский разработчик и производитель систем хранения данных. Готовые СХД для импортозамещения: блочные и объектные решения. Включены в реестры Минцифры и Минпромторга. Надёжность, проверенная годами.",
   icons: {
     icon: "/favicon.ico?v=3",
-  }
+  },
+  openGraph: {
+    type: "website",
+    locale: "ru_RU",
+    siteName: "ARGO.TECH",
+    url: "https://argo.tech",
+    title: "СХД РФ: Реестр Минцифры, Минпромторга для импортозамещения",
+    description:
+      "Российский разработчик и производитель систем хранения данных. Готовые СХД для импортозамещения: блочные и объектные решения. Включены в реестры Минцифры и Минпромторга. Надёжность, проверенная годами.",
+    images: [
+      {
+        url: "/hero-bg.jpg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ru" suppressHydrationWarning>
-      <body
-        className={`${oswald.variable} antialiased`}
-      >
+      <body className={`${oswald.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -37,7 +52,7 @@ export default function RootLayout({ children }) {
         >
           <ConsentProvider>
             <Header />
-              {children}
+            {children}
             <Footer />
             <CookieBanner />
             <Suspense fallback={null}>
